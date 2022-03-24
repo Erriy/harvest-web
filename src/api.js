c244973c941cecd7f8f2e71262290356758f160f
erriy <Erriy@msn.com>
2022-03-24 08:11:55 +0800

    feat: 🎸 注册接口对接调试

--- src/api.js
+++ src/api.js
@@ -1,12 +1,27 @@
+const sha512 = require('js-sha512');
+const axios = require('axios').default;
+
+const host = process.env.NODE_ENV === 'development' ? 'http://localhost:80/': window.location.origin + '/api';
+const axios_client = axios.create({
+    baseURL: host,
+});
 
 
 const user = {
+    password_hash(username, password) {
+        return sha512.sha512(`${username}:${password}@harvest`).toLowerCase();
+    },
     async regist({
         username='',
         password='',
         invite_code=''
     }={}) {
-        console.log(username, password, invite_code);
+        const res = await axios_client.post('/user/regist', {
+            username,
+            password: this.password_hash(username, password),
+            invite_code
+        });
+        console.log(res.data());
     },
     async login({
         username='',
