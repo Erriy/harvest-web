f9b6a9cc5d68b2983d30f7d358efbf0962b9c80c
erriy <Erriy@msn.com>
2022-04-04 16:57:34 +0800

    feat: 🎸 增加editor组件

---
+++ src/components/editor.vue
@@ -0,0 +1,142 @@
+<template>
+    <div id='editor'>
+        <a-row>
+            <a-textarea
+                v-model='seed.body.data'
+                placeholder="记录点什么"
+                :auto-size="{ minRows: 3, maxRows: 15}"
+            />
+        </a-row>
+        <div style='margin:15px'></div>
+        <a-row v-show='seed.body.data.length' type='flex'>
+            <a-col flex='auto'>
+                <a-tag
+                    v-for='(tag, ti) in seed.tags'
+                    :key='ti'
+                    closable
+                    @close='remove_tag(tag)'
+                >
+                    {{ tag }}
+                </a-tag>
+                <a-input
+                    v-if="tag.input.show"
+                    ref="tag_input"
+                    type="text"
+                    size="small"
+                    v-model="tag.input.value"
+                    :style="{ width: '78px', height: '21px' }"
+                    @blur="add_tag"
+                    @keyup.enter="add_tag"
+                />
+                <a-tag v-else style="background: #fff; borderStyle: dashed;" @click="show_tag_input">
+                    <a-icon type="plus" /> 添加标签
+                </a-tag>
+            </a-col>
+            <a-col flex='200px'>
+                <a-date-picker
+                    v-model="happen.time"
+                    :show-time='happen.show_time'
+                    :format="happen.format[happen.show_time?1:0]"
+                    placeholder="发生时间"
+                    style="position: relative; top: -5px;"
+                >
+                    <template slot="renderExtraFooter">
+                        <a-row>
+                            <a-col>
+                                <a-switch v-model='happen.show_time' checked-children="显示时间" un-checked-children="不显示时间" default-checked />
+                            </a-col>
+                        </a-row>
+                    </template>
+                    <span>
+                        <a-button
+                            type='link'
+                            style="position: relative; top: 5px;"
+                        >
+                            {{happen.time? happen.time: '添加事件发生时间'}}
+                        </a-button>
+                    </span>
+                </a-date-picker>
+            </a-col>
+            <a-col>
+                <a-button
+                    type='primary'
+                    :loading='publishing'
+                    @click='publish'
+                >
+                    发布
+                </a-button>
+            </a-col>
+        </a-row>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            happen: {
+                show_time: false,
+                format: [
+                    'YYYY-MM-DD',
+                    'YYYY-MM-DD HH:mm:ss',
+                ],
+                time: null,
+            },
+            tag: {
+                input: {
+                    show: false,
+                    value: '',
+                }
+            },
+            publishing: false,
+            seed: {
+                time: {
+                    happen: {
+                        value: null,
+                        accuracy: 'second',
+                    },
+                },
+                body: {
+                    data: '',
+                    type: 'text',
+                },
+                tags: [],
+            }
+        }
+    },
+    methods: {
+        show_tag_input() {
+            this.tag.input.value = ''
+            this.tag.input.show = true;
+            this.$nextTick(() => {
+                this.$refs.tag_input.focus();
+            });
+        },
+        add_tag() {
+            if(this.tag.input.value.length && !this.seed.tags.includes(this.tag.input.value)) {
+                this.seed.tags.push(this.tag.input.value);
+            }
+            this.tag.input.show = false;
+        },
+        remove_tag(tag) {
+            this.seed.tags.splice(this.seed.tags.indexOf(tag), 1);
+        },
+        async publish() {
+            this.publishing = true;
+            this.seed.time.happen.value = this.happen.time ? this.happen.time.valueOf() : null;
+            this.seed.time.happen.accuracy = this.happen.show_time ? 'second' : 'day';
+            try {
+                await this.$api.seed.publish([this.seed])
+            }
+            finally {
+                this.publishing = false;
+            }
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
