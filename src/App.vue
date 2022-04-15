86b6e4800c49f67af986a5b035f4cd807c042ea5
erriy <Erriy@msn.com>
2022-04-15 21:51:44 +0800

    feat: 🎸 所有页面都自动登陆

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+    <div id="app">
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
 
 export default {
-  name: 'App',
-  components: {
-  }
+    name: 'App',
+    components: {
+    },
+    mounted() {
+        this.$api.user.already_login().then(logined => {
+            if(logined) {
+                this.$message.success('已登录');
+                this.$router.push('/home');
+            }
+        });
+    }
 }
 </script>
 
--- src/components/login.vue
+++ src/components/login.vue
@@ -52,14 +52,7 @@ export default {
             }
         }
     },
-    mounted() {
-        this.$api.user.already_login().then(logined => {
-            if(logined) {
-                this.$message.success('已登录');
-                this.$router.push('/home');
-            }
-        });
-    }
+
 }
 </script>
 
